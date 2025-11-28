8cf062cc74633a2cb42efcd1d67453232d874224
Xinke Chen <xinkecf35@users.noreply.github.com>
2025-11-28 01:05:16 -0500

    add issuer url

--- global/iac/outputs.tf
+++ global/iac/outputs.tf
@@ -1,4 +1,9 @@
 
+output "okd_entra_id_issuer_url" {
+  description = "The Issuer URL for Entra ID SSO."
+  value = "https://login.microsoftonline.com/${data.azuread_client_config.current.tenant_id}/v2.0"
+}
+
 output "okd_entra_id_client_id" {
   description = "Application ID/Client ID for Entra ID SSO. For use with OpenShift OAuth."
   value       = azuread_application_registration.okd_cluster_app.client_id
