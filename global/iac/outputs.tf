b809ac738452f6ba44cccd7e3aa7bc72d8b1bcfd
Xinke Chen <xinkecf35@users.noreply.github.com>
2025-11-29 00:43:13 -0500

    change to using email

--- global/iac/outputs.tf
+++ global/iac/outputs.tf
@@ -1,16 +1,10 @@
 
-output "okd_entra_id_issuer_url" {
-  description = "The Issuer URL for Entra ID SSO."
-  value = "https://login.microsoftonline.com/${data.azuread_client_config.current.tenant_id}/v2.0"
-}
-
-output "okd_entra_id_client_id" {
-  description = "Application ID/Client ID for Entra ID SSO. For use with OpenShift OAuth."
-  value       = azuread_application_registration.okd_cluster_app.client_id
-}
-
-output "okd_entra_id_client_secret" {
-  description = "Application Password/Client Secret for Entra ID SSO. For use with OpenShift OAuth."
-  value       = azuread_application_password.okd_cluster_client_secret.value
-  sensitive  = true
+output "okd_entra_id_oidc_config" {
+  description = "A map of containing the issuer_url, client_id, and client_secret for Entra ID SSO for OKD/OpenShift OAuth."
+  sensitive   = true
+  value = {
+    client_id     = azuread_application_registration.okd_cluster_app.client_id
+    client_secret = azuread_application_password.okd_cluster_client_secret.value
+    issuer_url    = "https://login.microsoftonline.com/${data.azuread_client_config.current.tenant_id}/v2.0"
+  }
 }
